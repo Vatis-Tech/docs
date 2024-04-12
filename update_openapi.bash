083cd0db92d678c0011d2ce58371bfc04d62ad21
Alexandru Topala <alexandru.top98@gmail.com>
2024-04-12 14:28:07 +0300

    added open api initial schemas and update script

---
+++ update_openapi.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ! command -v jq >/dev/null 2>&1 ; then
+    echo "'jq' not found, please install it first"
+    exit 1
+fi
+
+if ! command -v curl >/dev/null 2>&1 ; then
+    echo "'curl' not found, please install it first"
+    exit 1
+fi
+
+# Update OpenAPI for HTTP Gateway
+curl https://http-gateway.vatis.tech/api-docs | \
+  jq '.components.schemas.ErrorResponse.properties.nestedErrors.items = {"type": "object"} | .servers = [{"url": "https://http-gateway.vatis.tech", "description": "Default server"}]' \
+  > api-reference/openapi/http-gateway.json
+
+# Update OpenAPI for Stream Configuration Service
+curl https://stream-configuration-service.vatis.tech/api-docs | \
+  jq '.components.schemas.ErrorResponse.properties.nestedErrors.items = {"type": "object"} | .servers = [{"url": "https://stream-configuration-service.vatis.tech", "description": "Default server"}]' \
+  > api-reference/openapi/stream-configuration-service.json
+
+# Update OpenAPI for Stream Service
+curl https://stream-service.vatis.tech/api-docs | \
+  jq '.components.schemas.ErrorResponse.properties.nestedErrors.items = {"type": "object"} | .servers = [{"url": "https://stream-service.vatis.tech", "description": "Default server"}]' \
+  > api-reference/openapi/stream-service.json
+
+# Update OpenAPI for Stream Service
+curl https://egress-service.vatis.tech/api-docs | \
+  jq '.components.schemas.ErrorResponse.properties.nestedErrors.items = {"type": "object"} | .servers = [{"url": "https://egress-service.vatis.tech", "description": "Default server"}]' \
+  > api-reference/openapi/egress-service.json
